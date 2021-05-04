fe47b4186fef1043b3adf565db0212a9bd6bb485
Charlotte366 <jc.oren@hotmail.com>
2021-05-04 15:14:51 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const images = document.querySelectorAll(".slider img");
 images.forEach(function(image){
     image.onclick = function(event) {
-        document.querySelector(".image-slider").classList.remove(".image-slider");
+        document.querySelector("image-slider").classList.remove("image-slider");
         const clickParent = event.target.parentNode;
         clickParent.classList.add("image-slider");
+        const clickParent = event.target.parentNode
+        clickParent.classList.add("image-slider")
     }
 })
