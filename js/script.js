81a49fd386e9d4d2ce7128703327ec12ab973f12
Charlotte366 <jc.oren@hotmail.com>
2021-05-04 13:39:13 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-
-
-
+const images = document.querySelectorAll(".slider img");
+images.forEach(function(image){
+    image.onclick = function(event) {
+        document.querySelector(".image-slider").classList.remove(".image-slider");
+        const clickParent = event.target.parentNode;
+        clickParent.classList.add("image-slider");
+    }
+})
