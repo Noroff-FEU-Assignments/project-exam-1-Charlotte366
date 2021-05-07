7b3bbbe09a61564eea44f3d4648009877d9dee99
Charlotte366 <jc.oren@hotmail.com>
2021-05-07 13:11:28 +0200

    update

--- js/blogpost.js
+++ js/blogpost.js
@@ -49,6 +49,7 @@ for (let i = 0; i < results.length; i++) {
     console.log(results[i].title);
     html +=  `<div class="post">
     <h2>${results[i].title.rendered}</h2>
+    <p>Date:${results[i].date}</p>
                 </div>
                 `;
 
