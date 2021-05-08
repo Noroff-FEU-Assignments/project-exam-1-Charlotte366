f96c1c6d705112f177b88496e9288be83314ec3b
Charlotte366 <jc.oren@hotmail.com>
2021-05-09 01:35:05 +0200

    Update spesificblogpost.js

--- js/spesificblogpost.js
+++ js/spesificblogpost.js
@@ -20,6 +20,7 @@ const proxy = "https://noroffcors.herokuapp.com/";
 
 
 
+/*
 const url = "https://myblog.charlotte366.no/wp-json/wp/v2/posts/?" 
 
 const proxy = "https://noroffcors.herokuapp.com/";
@@ -42,6 +43,54 @@ console.log(results);
 }
 
 fetchArray()
+
+*/
+
+/*
+const url = 'https://myblog.charlotte366.no/wp-json/wp/v2/posts/59'
+const containerSpesific = document.querySelector(".blogs");
+
+async function getBlogpost() {
+	const response = await fetch (url);
+	const result = await response.json();
+	console.log(result)
+	
+	
+containerSpesific.innerHTML = result.title.rendered
+
+}
+
+	getBlogpost();
+
+    */
+
+    const containerSpesific = document.querySelector(".blogs");
+
+    const queryString = document.location.search;
+    const params = new URLSearchParams(queryString);
+    const id = params.get("id");
+    console.log(id);
+    
+    const url ="https://myblog.charlotte366.no/wp-json/wp/v2/posts/" + id;
+    console.log(url);
+    
+    const proxy = "https://noroffcors.herokuapp.com/"; 
+    const corsFix = proxy + url;
+    
+    
+    
+    
+    async function getBlogpost() {
+        const response = await fetch (url);
+        const result = await response.json();
+        console.log(result)
+        
+        
+    containerSpesific.innerHTML = result.content.rendered
+    
+    }
+    
+        getBlogpost();   
 
 
 
