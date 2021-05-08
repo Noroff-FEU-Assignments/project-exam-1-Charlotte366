95d1b68c18add78714ff8424edd426726413ed64
Charlotte366 <jc.oren@hotmail.com>
2021-05-08 14:40:10 +0200

    update

--- js/spesificblogpost.js
+++ js/spesificblogpost.js
@@ -1,3 +1,4 @@
+/*
 const containerSpesific = document.querySelector(".blogs");
 
 const queryString = document.location.search;
@@ -13,6 +14,34 @@ const url ="https://myblog.charlotte366.no/wp-json/wp/v2/posts" + results;
 console.log(url);
 
 const proxy = "https://noroffcors.herokuapp.com/"; 
+
+*/
+
+
+
+
+const url = "https://myblog.charlotte366.no/wp-json/wp/v2/posts/?" 
+
+const proxy = "https://noroffcors.herokuapp.com/";
+
+const corsFix = proxy + url;
+
+
+
+async function fetchArray(){
+
+
+const response = await fetch (corsFix);
+
+const results = await response.json();
+
+
+
+console.log(results);
+
+}
+
+fetchArray()
 
 
 
